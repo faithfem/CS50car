03185c93e6939f68728ee18923c2912c4263e773
Faith <fmugure@intracitygeeks.org>
2017-11-13 04:45:12 +0000

    caesar complete

--- pset2/caesartest.c
+++ pset2/caesartest.c
@@ -10,19 +10,18 @@ int main(int argc, string argv[])
     if (argc != 2)
     {
     printf("Usage: ./caesar k\n");
-    return 1;
     }
     else{
     int k = atoi(argv[1]);
-    printf("plaintext: ");
+    printf("Plaintext: ");
     string userWord = get_string();
-    printf("ciphertext: ");
+    printf("Ciphertext: ");
 
     for (int i = 0; i < strlen(userWord); i++)
-    {       if (isalpha(userWord[i]))           // Check if character is alpha
-            {   if(isupper(userWord[i]))        // Check if user input is in upper or lower case
+    {       if (isalpha(userWord[i]))
+            {   if(isupper(userWord[i]))
                 {
-                 printf("%c", ((userWord[i] - 'A' + k))%26+'A'); // Character less value of A which is 65 plus the key,
+                 printf("%c", ((userWord[i] - 'A' + k))%26+'A');
                 }
                 else
                 {
