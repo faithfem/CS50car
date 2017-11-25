d9f08a0e47d8e884e1348fa7a74e612e23cbded7
Faith <fmugure@intracitygeeks.org>
2017-11-25 02:53:46 +0000

    find complete, passed check50

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -3,7 +3,7 @@
  *
  * Helper functions for Problem Set 3.
  */
- 
+
 #include <cs50.h>
 
 #include "helpers.h"
@@ -14,14 +14,56 @@
 bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
-    return false;
+    if (value < 0)
+    {
+        return false;
+    }
+        int smallest = 0, largest = n - 1; // Set smallest and largest numbers between 0 and n
+
+        while (n > 0)
+        {
+            int middle = (largest - smallest)/2 + smallest;
+            {
+                if (values[middle] == value)
+                {
+                    return true;
+                }
+                 else if (values[middle] > value)
+                {
+                    largest = middle - 1;
+                }
+                    else if (values[middle] < value)
+                {
+                smallest = middle +1;
+                }
+            }
+            n = largest - smallest + 1;
+        }
+        return false;
 }
+
 
 /**
  * Sorts array of n values.
  */
-void sort(int values[], int n)
+void sort(int values[], int n) //Selection sort
 {
     // TODO: implement a sorting algorithm
-    return;
+        for (int i = 0; i < n-1; i++)
+        {
+            int smallest = i;
+            for (int x = i+1; x < n; x++)
+            {
+                if (values[smallest] > values[x])
+                    smallest = x;
+            }
+                //if (smallest != i);
+                // {
+                    int newSmallest = values[smallest];
+                    values[smallest] = values[i];
+                    values [i] = newSmallest;
+                // }
+            // }
+            // return;
+        }
 }
